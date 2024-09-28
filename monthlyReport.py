de61b818d041ae01233b75fbff526d54553f3cf5
isabelruizph <isabelruizph03@gmail.com>
2024-09-28 18:39:49 -0400

    Adding data and analytics

--- monthlyReport.py
+++ monthlyReport.py
@@ -1,10 +1,10 @@
-def generate_monthly_report(monthly_spending):
-    report = "----- Your Monthly Spending Report -----\n"
+def generate_monthly_report(monthly_spending): 
+    report = "Your Monthly Spending Report\n" # Header for report
     
-    for month, categories in monthly_spending.items():
-        report += f"\nMonth: {month}\n"
-        for category, amount in categories.items():
-            report += f"- {category}: ${amount:.2f}\n"
+    for month, categories in monthly_spending.items(): #loop to iterate over monthly spending returning month and category
+        report += f"\nMonth: {month}\n" #append the current month to the report string
+        for category, amount in categories.items(): # this loop goes through each category and returns amount spent in that month
+            report += f" {category}: ${amount:.2f}\n" # appends category and amount spent to string retun
 
     report += "\n\nOverall Insights:\n"
     insights = generate_insights(monthly_spending)
